a4bfb52afca212e511420326144e7541cc02768b
Rafael <rafaelrodrigues@bee.com.br>
2023-08-27 10:09:11 -0300

    up

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyrigth(){
+    return(
+        <div className='text-sm leading-relaxed text-gray-200'>
+          Feito com s2 no NLW da{' '}
+          <a 
+            target='_blank'
+            href=""
+            rel='noreferrer'
+            className='underline hover:text-gray-100'
+          >
+            Rocketseat
+          </a>
+        </div>
+    )
+}
